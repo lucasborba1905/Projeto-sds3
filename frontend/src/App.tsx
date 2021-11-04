9539f7f69553e700dba366e96843bc008c203457
lucas borba <manoborba19@gmail.com>
2021-11-03 23:48:30 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import Datatable from "componets/Datatable";
+import Footer from "componets/Footer";
+import NavBar from "componets/NavBar";
+
 function App() {
   return (
-    <div> 
-      <h1 className="text-primary" > Olá, Mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary" > Olá, Mundo!</h1>
+        <Datatable />
+      </div>
+     <Footer />
+    </>
   );
 }
 
